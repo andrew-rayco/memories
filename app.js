37c95373a51b64a31fccfd9a98ba1ae4b9b2c517
Andrew <andy@andycondon.com>
2017-11-30 17:02:59 +1300

    static files and css setup

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express')
+var path = require('path')
 
 var app = express()
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('hello redux and react')
+  res.render('index')
 })
 
 app.listen(3000, () => {
